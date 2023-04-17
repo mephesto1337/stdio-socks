e784ee1467be4d0de078033b9e313e877ca7a5c2
Thomas WACHE <thomas@wache.fr>
2023-04-17 13:19:20 +0200

    Set Custom endpoint to a generic type

--- src/proto/endpoint.rs
+++ src/proto/endpoint.rs
@@ -11,15 +11,24 @@ use nom::multi::length_data;
 use nom::number::streaming::{be_u16, be_u32, be_u8};
 use nom::sequence::{preceded, tuple};
 
+#[derive(Debug, Clone, PartialEq, Eq, Copy)]
+pub struct EmptyCustom;
+
+impl fmt::Display for EmptyCustom {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("\u{2205}")
+    }
+}
+
 /// Different endpoints to connect to
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Endpoint {
+pub enum Endpoint<C = EmptyCustom> {
     /// An UNIX socket (STREAM, not DGRAM)
     UnixSocket { path: String },
     /// A TCP endpoint
     TcpSocket { address: Address, port: u16 },
     /// A Custom endpoint
-    Custom { specs: Vec<u8> },
+    Custom(C),
 }
 
 impl From<SocketAddr> for Endpoint {
@@ -41,7 +50,10 @@ const ENDPOINT_TYPE_UNIX: u8 = 1;
 const ENDPOINT_TYPE_TCP: u8 = 2;
 const ENDPOINT_TYPE_CUSTOM: u8 = 3;
 
-impl Wire for Endpoint {
+impl<C> Wire for Endpoint<C>
+where
+    C: Wire,
+{
     fn encode_into(&self, buffer: &mut Vec<u8>) {
         match self {
             Self::UnixSocket { ref path } => {
@@ -53,11 +65,9 @@ impl Wire for Endpoint {
                 address.encode_into(buffer);
                 buffer.extend_from_slice(&port.to_be_bytes()[..]);
             }
-            Self::Custom { specs } => {
+            Self::Custom(ref c) => {
                 buffer.push(ENDPOINT_TYPE_CUSTOM);
-                let size: u32 = specs.len().try_into().expect("Cannot fit size in u32");
-                buffer.extend_from_slice(&size.to_be_bytes()[..]);
-                buffer.extend_from_slice(&specs[..]);
+                c.encode_into(buffer)
             }
         }
     }
@@ -81,27 +91,51 @@ impl Wire for Endpoint {
                 ),
                 preceded(
                     verify(be_u8, |b| *b == ENDPOINT_TYPE_CUSTOM),
-                    map(length_data(be_u32), |data: &[u8]| Self::Custom {
-                        specs: data.to_vec(),
-                    }),
+                    map(C::decode, Self::Custom),
                 ),
             )),
         )(buffer)
     }
 }
 
-impl fmt::Display for Endpoint {
+impl Wire for EmptyCustom {
+    fn encode_into(&self, _buffer: &mut Vec<u8>) {}
+
+    fn decode<'i, E>(buffer: &'i [u8]) -> nom::IResult<&'i [u8], Self, E>
+    where
+        E: nom::error::ParseError<&'i [u8]> + nom::error::ContextError<&'i [u8]>,
+    {
+        Ok((buffer, Self))
+    }
+}
+
+impl Wire for Vec<u8> {
+    fn encode_into(&self, buffer: &mut Vec<u8>) {
+        let size: u32 = self
+            .len()
+            .try_into()
+            .expect("Vec's length does not fit into u32");
+        buffer.extend_from_slice(&size.to_be_bytes()[..]);
+        buffer.extend_from_slice(&self[..]);
+    }
+
+    fn decode<'i, E>(buffer: &'i [u8]) -> nom::IResult<&'i [u8], Self, E>
+    where
+        E: nom::error::ParseError<&'i [u8]> + nom::error::ContextError<&'i [u8]>,
+    {
+        context("Vec", map(length_data(be_u32), |data: &[u8]| data.to_vec()))(buffer)
+    }
+}
+
+impl<C> fmt::Display for Endpoint<C>
+where
+    C: fmt::Display,
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::UnixSocket { ref path } => write!(f, "unix://{}", path),
             Self::TcpSocket { ref address, port } => write!(f, "tcp://{}:{}", address, port),
-            Self::Custom { ref specs } => {
-                f.write_str("custom://0x")?;
-                for b in &specs[..] {
-                    write!(f, "{b:02x}")?;
-                }
-                Ok(())
-            }
+            Self::Custom(ref c) => write!(f, "custom://{c}"),
         }
     }
 }
