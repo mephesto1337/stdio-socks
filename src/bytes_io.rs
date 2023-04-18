39c175cdd89fcf97da1de68a2800da3c3d0da501
Thomas WACHE <thomas@wache.fr>
2023-04-18 13:54:42 +0200

    Added BytesIO

---
+++ src/bytes_io.rs
@@ -0,0 +1,58 @@
+use std::{
+    io,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use tokio::io::{AsyncRead, AsyncWrite};
+
+/// A read-only view of bytes
+#[derive(Debug, Default)]
+pub struct BytesIO {
+    buffer: Vec<u8>,
+    offset: usize,
+}
+
+impl BytesIO {
+    pub fn new(data: impl AsRef<[u8]>) -> Self {
+        Self {
+            buffer: data.as_ref().to_vec(),
+            offset: 0,
+        }
+    }
+}
+
+impl AsyncRead for BytesIO {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        if self.offset < self.buffer.len() {
+            let rest = &self.buffer[self.offset..];
+            let n = buf.remaining().min(rest.len());
+            buf.put_slice(&rest[..n]);
+            self.get_mut().offset += n;
+        }
+
+        Poll::Ready(Ok(()))
+    }
+}
+
+impl AsyncWrite for BytesIO {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        Poll::Ready(Ok(buf.len()))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,8 @@ pub use stdio::Stdio;
 mod empty;
 pub use empty::DevNull;
 
+mod bytes_io;
+pub use bytes_io::BytesIO;
+
 mod multiplexer;
 pub use multiplexer::{Channel, Multiplexer, OpenStreamResult, Stream};
